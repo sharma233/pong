5d98cef68c054d4a2e8ac5e6eef3f9f094bb29c7
Surya Sharma <surya.sharma233@gmail.com>
2023-02-25 19:29:36 -0600

    ugly scoring implementation

--- pong.cpp
+++ pong.cpp
@@ -1,10 +1,32 @@
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <stdio.h>
 #include <string.h>
+#include <string>
 #include <cmath>
+
+//could inherit some kind of texture interface
+class Score {
+public:
+	Score();
+	~Score();
+	void render(int x, int y);
+	void setScore(int newScore);
+	void incrementScore();
+	void free();
+private:
+	int mScore;
+	int mWidth;
+	int mHeight;
+	SDL_Texture* mTexture;
+	void loadTexture();
+};
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+Score gPlayerScore;
+Score gOppScore;
+TTF_Font* gFont = NULL;
 
 //starts up SDL and creates window
 bool init();
@@ -113,12 +135,18 @@ Ball::Ball() {
 
 
 void Ball::move(SDL_Rect& player_paddle_collider, SDL_Rect& opp_paddle_collider) {
+	//scoring logic, should also get moved out of here
 	mPosX += mVelX;
-	if (mPosX < 0 || mPosX + BALL_WIDTH > SCREEN_WIDTH) {
-		mPosX -= mVelX;
+	if (mPosX < 0) {
+		gOppScore.incrementScore();
+		mPosX = SCREEN_WIDTH / 2;
 		mVelX = -mVelX;
 	}
-	mCollider.x = mPosX;
+	if (mPosX + BALL_WIDTH > SCREEN_WIDTH) {
+		gPlayerScore.incrementScore();
+		mPosX = SCREEN_WIDTH / 2;
+		mVelX = -mVelX;
+	}
 
 	mPosY += mVelY;
 	if (mPosY < 0 || mPosY + BALL_HEIGHT > SCREEN_HEIGHT) {
@@ -126,10 +154,10 @@ void Ball::move(SDL_Rect& player_paddle_collider, SDL_Rect& opp_paddle_collider)
 		mVelY = -mVelY;
 	}
 
+	//should get moved out of here
 	if (checkCollision(mCollider, player_paddle_collider) || checkCollision(mCollider, opp_paddle_collider)) {
-		mPosY -= mVelY;
-		mPosX -= mVelX;
-		//mVelY = -mVelY;
+		mPosY -= mVelY*2;
+		mPosX -= mVelX*2;
 		mVelX = -mVelX;
 	}
 	mCollider.x = mPosX;
@@ -142,27 +170,80 @@ void Ball::render() {
 	SDL_RenderFillRect(gRenderer, &fillRect);
 }
 
+
+
+Score::Score() {
+	mScore = 0;
+	mWidth = 0;
+	mHeight = 0;
+	mTexture = NULL;
+}
+
+Score::~Score() {
+	free();
+}
+
+void Score::loadTexture() {
+	free();
+	SDL_Color color = {255, 255, 255};
+	SDL_Surface* surface = TTF_RenderText_Solid(gFont, std::to_string(mScore).c_str(), color);
+	mTexture = SDL_CreateTextureFromSurface(gRenderer, surface);
+	mWidth = surface->w;
+	mHeight = surface->h;
+	SDL_FreeSurface(surface);
+}
+void Score::free() {
+	if (mTexture != NULL) {
+		SDL_DestroyTexture(mTexture);
+		mTexture = NULL;
+		mWidth = 0;
+		mHeight = 0;
+	}
+ }
+
+void Score::render(int x, int y) {
+	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+	SDL_RenderCopyEx(gRenderer, mTexture, NULL, &renderQuad, NULL, NULL, SDL_FLIP_NONE);
+}
+
+void Score::setScore(int newScore) {
+	mScore = newScore;
+	loadTexture();
+}
+
+void Score::incrementScore() {
+	setScore(mScore + 1);
+}
+
 bool init() {
 	bool success = true;
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("SDL could not be loaded, SDL ERROR: %s\n", SDL_GetError());
 		success = false;
-	}
-	else {
+	} else {
 		gWindow = SDL_CreateWindow("Pong!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (gWindow == NULL) {
 			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
 			success = false;
-		}
-		else {
+		} else {
 			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
 			if (gRenderer == NULL) {
 				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
 				success = false;
-			}
-			else {
+			} else {
 				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+				if (TTF_Init() == -1)
+				{
+					printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+					success = false;
+				} else {
+					gFont = TTF_OpenFont("lazy.ttf", 28);
+					if (gFont == NULL) {
+						printf("Font could not be created! SDL Error: %s\n", SDL_GetError());
+						success = false;
+					}
+				}
 			}
 		}
 	}
@@ -244,6 +325,8 @@ int main(int argc, char* args[]) {
 			player_paddle.render();
 			opp_paddle.render();
 			ball.render();
+			gPlayerScore.render(SCREEN_WIDTH/4, SCREEN_HEIGHT/4);
+			gOppScore.render(SCREEN_WIDTH / 1.5, SCREEN_HEIGHT / 4);
 
 			//update screen
 			SDL_RenderPresent(gRenderer);
